4117a74d06535d3897ad5d95309c87b577717b2a
luobochuanqi <luobochuanqi@qq.com>
2023-03-17 00:42:25 +0800

    complete DrillBlock✔✔

--- src/main/java/xyz/luobochuanqi/mindustry/common/world/BlockEntity/Machine/Mechanical_Drill/MechanicalDrillBlock.java
+++ src/main/java/xyz/luobochuanqi/mindustry/common/world/BlockEntity/Machine/Mechanical_Drill/MechanicalDrillBlock.java
@@ -3,6 +3,7 @@ package xyz.luobochuanqi.mindustry.common.world.BlockEntity.Machine.Mechanical_D
 import net.minecraft.block.BlockState;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.ServerPlayerEntity;
+import net.minecraft.inventory.InventoryHelper;
 import net.minecraft.inventory.container.INamedContainerProvider;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.ActionResultType;
@@ -12,7 +13,9 @@ import net.minecraft.util.math.BlockRayTraceResult;
 import net.minecraft.world.IBlockReader;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.network.NetworkHooks;
+import net.minecraftforge.items.CapabilityItemHandler;
 import xyz.luobochuanqi.mindustry.common.Type.DrillBlock.DrillBlock;
+import xyz.luobochuanqi.mindustry.common.util.Mod2x2Part;
 
 import javax.annotation.Nullable;
 
@@ -43,5 +46,15 @@ public class MechanicalDrillBlock extends DrillBlock {
             }
             return ActionResultType.CONSUME;
         }
+    }
+
+    @Override
+    public void onRemove(BlockState pState, World pLevel, BlockPos pPos, BlockState pNewState, boolean pIsMoving) {
+        if (!pState.is(pNewState.getBlock()) && pState.getValue(ModPART) == Mod2x2Part.START) {
+            pLevel.getBlockEntity(pPos).getCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY, null).ifPresent(cap -> {
+                InventoryHelper.dropItemStack(pLevel, pPos.getX(), pPos.getY(), pPos.getZ(), cap.getStackInSlot(0));
+            });
+        }
+        super.onRemove(pState, pLevel, pPos, pNewState, pIsMoving);
     }
 }
