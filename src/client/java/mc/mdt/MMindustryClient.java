fbe73e6394308df1f1314a9d7396429a1abd69da
luobochuanqi <luobochuanqi@qq.com>
2023-08-08 18:01:22 +0800

    revert: 初始化fabric仓库

---
+++ src/client/java/mc/mdt/MMindustryClient.java
@@ -0,0 +1,13 @@
+package mc.mdt;
+
+import net.fabricmc.api.ClientModInitializer;
+
+/**
+ * @author luobochuanqi
+ */
+public class MMindustryClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/main/java/mc/mdt/MMindustryDataGenerator.java
@@ -0,0 +1,11 @@
+package mc.mdt;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class MMindustryDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
